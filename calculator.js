21e270d67bcc147256282b2a77f3377de406b2e2
possible <sumitkumar66416@gmail.com>
2022-05-09 15:10:28 +0700

    first commit

---
+++ calculator.js
@@ -0,0 +1,23 @@
+let string = "";
+let button = document.querySelectorAll(".btn");
+Array.from(button).forEach((button) => {
+    button.addEventListener('click',(e) => {
+        if (e.target.innerHTML == ' =') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == "C"){
+            string = " "
+            document.getElementById('number').value = string;
+        }
+        else if (e.target.innerHTML == "PI") {
+            string = string + 3.14
+            document.getElementById('number').value = string;
+        }
+        else{
+            string = string + e.target.innerHTML;
+            document.getElementById('number').value = string;
+
+        }
+    })
+})
